36e478edb26af036e67e746975c07f5ec6cd0bec
jaylinsg <jstarlin@bu.edu>
2025-08-03 20:13:09 -0400

    mp-5: deployment fix

--- mp-5/app/r/[alias]/route.ts
+++ mp-5/app/r/[alias]/route.ts
@@ -1,22 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 import clientPromise from "@/db";
 
 export async function GET(
-    req: NextRequest,
-    { params }: { params: { alias: string } }
+  req: NextRequest,
+  context: any
 ) {
-    const alias = params.alias;
+  const alias = context.params.alias;
 
-    const client = await clientPromise;
-    const db = client.db("shortener");
-    const collection = db.collection("urls");
+  const client = await clientPromise;
+  const db = client.db("shortener");
+  const collection = db.collection("urls");
 
-    const result = await collection.findOne({ alias });
+  const result = await collection.findOne({ alias });
 
-    if (result && result.url) {
-        redirect(result.url);
-    } else {
-        return new Response("Alias not found", { status: 404 });
-    }
+  if (result && result.url) {
+    redirect(result.url);
+  } else {
+    return new Response("Alias not found", { status: 404 });
+  }
 }
